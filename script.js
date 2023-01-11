2ff9176499d94f263842efa9719dabcb74330ff6
Aduttya <55932256+aduttya@users.noreply.github.com>
2023-01-11 20:07:41 +0530

    file added

---
+++ script.js
@@ -0,0 +1,9 @@
+  // Select the post text and get the word count
+  var postText = document.querySelector('.post-text').textContent;
+  var wordCount = postText.split(' ').length;
+  
+  // Calculate the read time based on an average reading speed of 200 words per minute
+  var readTime = Math.ceil(wordCount / 200);
+  
+  // Display the read time in the .read-time element
+  document.querySelector('.read-time').innerHTML = readTime + ' min';

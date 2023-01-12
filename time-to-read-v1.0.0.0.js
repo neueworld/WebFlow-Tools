9a7e9e382adc2fbef652031b832445c1c8deed5e
abhayrawat-26 <122516262+abhayrawat-26@users.noreply.github.com>
2023-01-13 03:22:47 +0530

    Update time-to-read-v1.0.0.0.js

--- time-to-read-v1.0.0.0.js
+++ time-to-read-v1.0.0.0.js
@@ -5,7 +5,7 @@ var postText = document.querySelector('[read-text]').textContent.split(' ').leng
 var readTime = Math.ceil(postText / 200);
   
 // Calculate a variable for text that would take a minute or less to be read
-var readingTime = readTime > 1 ? readTime + ' minutes' : "1 minute";
+var readingTime = readTime > 1 ? readTime + ' minutes' : '1 minute';
   
 // Display the read time 
 document.querySelector('[time-to-read]').innerHTML = readingTime;

814bc1b1e0dd03face7e84145018c1eaaca7ba15
abhayrawat-26 <122516262+abhayrawat-26@users.noreply.github.com>
2023-01-13 03:03:06 +0530

    Update time-to-read-v1.0.0.0.js

--- time-to-read-v1.0.0.0.js
+++ time-to-read-v1.0.0.0.js
@@ -1,9 +1,11 @@
-// Select the post text and get the word count
-  var postText = document.querySelector('.post-text').textContent;
-  var wordCount = postText.split(' ').length;
+// Select the text to read and get the word count
+var postText = document.querySelector('[read-text]').textContent.split(' ').length;
   
-  // Calculate the read time based on an average reading speed of 200 words per minute
-  var readTime = Math.ceil(wordCount / 200);
+// Calculate the read time based on an average reading speed of 200 words per minute
+var readTime = Math.ceil(postText / 200);
   
-  // Display the read time in the .read-time element
-  document.querySelector('.read-time').innerHTML = readTime + ' min';
+// Calculate a variable for text that would take a minute or less to be read
+var readingTime = readTime > 1 ? readTime + ' minutes' : "1 minute";
+  
+// Display the read time 
+document.querySelector('[time-to-read]').innerHTML = readingTime;

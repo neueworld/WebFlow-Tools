e8ba5c08f55f6d31cb9f2fc21ff4aa64c445c815
abhayrawat-26 <122516262+abhayrawat-26@users.noreply.github.com>
2023-01-13 16:53:02 +0530

    Update copy-to-clipboard-v1.0.0.0.js

--- copy-to-clipboard-v1.0.0.0.js
+++ copy-to-clipboard-v1.0.0.0.js
@@ -1,1 +1,20 @@
-
+// Copy to Clipboard
+var copyButtons = document.querySelectorAll("[copy-button]");
+copyButtons.forEach(function(copyButton) {
+copyButton.addEventListener("click", function() {
+var clickedElement = this;
+var parentElement = clickedElement.parentNode;
+var articleElement = parentElement.querySelector("[read-text]");
+if (articleElement) {
+var text = articleElement.textContent;
+var tempInput = document.createElement("input");
+tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+tempInput.value = text;
+document.body.appendChild(tempInput);
+tempInput.select();
+document.execCommand("copy");
+document.body.removeChild(tempInput);
+this.textContent = "Copied!";
+}
+});
+});
